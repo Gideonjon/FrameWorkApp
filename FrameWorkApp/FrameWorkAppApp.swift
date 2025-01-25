e6e681a2f77346679b1f735777903f4973011841
Gideonjon <gideonjones63@gmail.com>
2025-01-26 00:40:28 +0100

    Initial Commit

---
+++ FrameWorkApp/FrameWorkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameWorkAppApp.swift
+//  FrameWorkApp
+//
+//  Created by mac on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FrameWorkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
