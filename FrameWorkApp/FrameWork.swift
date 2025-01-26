90430222c3f4cd9ad73f5cfb12c3ad977719c6a4
Gideonjon <gideonjones63@gmail.com>
2025-01-26 02:25:53 +0100

    new-files-added-now

---
+++ FrameWorkApp/FrameWork.swift
@@ -0,0 +1,8 @@
+//
+//  FrameWork.swift
+//  FrameWorkApp
+//
+//  Created by mac on 26/01/2025.
+//
+
+import Foundation
---
+++ FrameWorkApp/FrameworkGridView .swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView .swift
+//  FrameWorkApp
+//
+//  Created by mac on 26/01/2025.
+//
+
+import SwiftUI
+
+struct FrameworkGridView_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView_()
+}
