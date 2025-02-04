221077c766a686ed6c4e399f3ca4e2e3d8029c63
Gideonjon <gideonjones63@gmail.com>
2025-02-04 22:54:49 +0100

    detail-screen-added-now

---
+++ FrameWorkApp/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  FrameWorkApp
+//
+//  Created by mac on 04/02/2025.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
