818ba1f9c5d4d9c0a60ba57ca2460d229b1c986b
Gideonjon <gideonjones63@gmail.com>
2025-02-05 04:59:16 +0100

    new-button-screen-added

---
+++ FrameWorkApp/AFButtoon.swift
@@ -0,0 +1,18 @@
+//
+//  AFButtoon.swift
+//  FrameWorkApp
+//
+//  Created by mac on 05/02/2025.
+//
+
+import SwiftUI
+
+struct AFButtoon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButtoon()
+}
